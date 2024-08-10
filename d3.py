ce7d63dea4470a4de1ada79b0d7a3e9b4ca48a6d
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 02:05:38 +0530

    Add files via upload

---
+++ d3.py
@@ -0,0 +1,53 @@
+# intro to string
+# name = "himanshu"
+# nameshort = name[0:3] # start from index 0 all the way till 3 (excluding 3)
+# print(nameshort)
+# character1 = name[1]
+# print(character1)
+
+# negative slicing 
+# name="himanshu"
+# # print(name[-1:-5])
+# print(name[-1:-5])
+ 
+# string function
+# print(len(name))
+# print(name.endswith("rry"))
+# print(name.startswith("ha"))
+# print(name.capitalize())
+
+# escape sequence charator in python 
+# a = 'Harry is a good boy\nbut not a bad \'boy\''
+# print(a)
+
+
+# practice set of day-3
+# 1
+# name=input("enter your name:-")
+# print(name,", Good Afternoon")
+
+#2
+# name=input("enter your name:-")
+# Date=input("enter date:-")
+# letter=f"""
+#            Dear {name}
+#            you are selected
+#            {Date}
+#   """
+           
+# print(letter)
+
+# 3
+# str="hi, i am  himanshu."
+# findstr=str.find("  ")
+# print(findstr)
+
+# 4
+# str="hi, i am  himanshu."
+# newstr=str.replace("  "," ")
+# print(str)
+# print(newstr)
+
+# 5
+# str="Dear Harry,\n\tthis course is nice.\nThanks!"
+# print(str)
