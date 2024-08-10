9a17ba7b9dd5d8b4427ba32d5f9ba96eac6fa64a
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 01:49:31 +0530

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,59 @@
+# print("hello world")
+
+
+# import pyjokes
+
+# # print("Printing Jokes...")
+
+# # This prints a random joke
+# joke = pyjokes.get_joke()
+# print(joke)
+ 
+
+# practise set-1
+# 1
+# print(''' Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# When the blazing sun is gone,
+# When he nothing shines upon,
+# Then you show your little light,
+# Twinkle, twinkle, all the night.
+
+# Then the trav'ller in the dark,
+# Thanks you for your tiny spark,
+# He could not see which way to go,
+# If you did not twinkle so.
+
+# In the dark blue sky you keep,
+# And often thro' my curtains peep,
+# For you never shut your eye,
+# Till the sun is in the sky.
+
+# 'Tis your bright and tiny spark,
+# Lights the trav'ller in the dark:
+# Tho' I know not what you are,
+# Twinkle, twinkle, little star.''')
+
+# 2
+# Done using REPL
+
+# 3
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("Hey I am good")
+# engine.runAndWait()
+
+# 4(using chatgpt)
+# import os
+
+# # Select the directory whose content you want to list 
+# directory_path = '/'
+
+# # Use the os module to list the directory content 
+# contents = os.listdir(directory_path)
+
+# # Print the contents of the directory 
+# print(contents)
