1eeaa094e06a34808345ef6536aeb6be2258057b
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 02:12:28 +0530

    Add files via upload

---
+++ d4.py
@@ -0,0 +1,86 @@
+# intro to list 
+# friends = ["Apple", "Orange", 5, 345.06, False, "Aakash", "Rohan"]
+# print(friends[0])
+# friends[0] = "Grapes" # Unlike Strings lists are mutable
+# print(friends[0])
+# print(friends[1:4])
+
+# list function 
+# friends.append("Harry")
+# print(friends)
+
+# l1 = [1, 34,62, 2, 6, 11]
+# # l1.sort()
+# # l1.reverse()
+# # l1.insert(2, 333333) # Insert 333333 such that its index in the list is 3
+# value = l1.pop(3)
+# print(value)
+# print(l1)
+
+# intro to tuple
+# a = (1,45,342,3424,False, "Rohan", "Shivam")
+# print(a)
+# print(type(a))
+
+# tuple function 
+# a = (1,45,342,3424,False, 45, "Rohan", "Shivam")
+# print(a) 
+# no = a.count(45)
+# print(no)
+# i = a.index(3424)
+# print(i)
+# print(len(a))
+
+
+
+
+
+# practice set of day-4
+# list=[]
+# f1=input("enter fruit name:-")
+# f2=input("enter fruit name:-")
+# f3=input("enter fruit name:-")
+# f4=input("enter fruit name:-")
+# f5=input("enter fruit name:-")
+# f6=input("enter fruit name:-")
+# f7=input("enter fruit name:-")
+# list.append(f1)
+# list.append(f2)
+# list.append(f3)
+# list.append(f4)
+# list.append(f5)
+# list.append(f6)
+# list.append(f7)
+# print(list)
+
+# 2
+# list1=[]
+# std1=int(input("enter your mark;-"))
+# std2=int(input("enter your mark;-"))
+# std3=int(input("enter your mark;-"))
+# std4=int(input("enter your mark;-"))
+# std5=int(input("enter your mark;-"))
+# std6=int(input("enter your mark;-"))
+# list1.append(std1)
+# list1.append(std2)
+# list1.append(std3)
+# list1.append(std4)
+# list1.append(std5)
+# list1.append(std6)
+# list1.sort()
+# print(list1)
+
+# 3
+# tpl=(1,4,7,8)
+# tpl[1]=2 #throug an error
+# print(tpl)
+
+# 4
+# l2=[1,3,5,6,7,3]
+# total=l2[0]+l2[1]+l2[2]+l2[3]+l2[4]+l2[5]
+# print(total)
+
+# 5
+# a=(7,0,8,0,0,9)
+# zeros=a.count(0)
+# print(zeros)
