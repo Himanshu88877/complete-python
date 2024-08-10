81ec9fef506a365e46d27fe443948c273668226c
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 02:35:15 +0530

    Add files via upload

---
+++ d6.py
@@ -0,0 +1,119 @@
+# intro to coditional python
+# a = int(input("Enter your age: "))
+
+# # If else statement
+# if(a>=18):
+#     print("You are above the age of consent")
+#     print("Good for you")
+
+# else:
+#     print("You are below the age of consent")
+
+
+# print("End of Program")
+
+# if elif else 
+# a = int(input("Enter your age: "))
+
+# # If elif else ladder
+# if(a>=18):
+#     print("You are above the age of consent")
+#     print("Good for you")
+
+# elif(a<0):
+#     print("You are entering an invalid negative age")
+
+# elif(a==0):
+#     print("You are entering 0 which is not a valid age")    
+
+# else:
+#     print("You are below the age of consent")
+
+
+# print("End of Program")
+
+
+
+
+
+
+
+
+
+# practice set of day-6
+# 1
+# num1=int(input("enter a number:-"))
+# num2=int(input("enter a number:-"))
+# num3=int(input("enter a number:-"))
+# num4=int(input("enter a number:-"))
+# if(num1>=num2 and num1>=num3 and num1>=num4):
+#   print("greatest num is:-",num1)
+# elif(num2>=num1 and num2>=num3 and num2>=num4):
+#   print("greatest num is:-",num2)
+# elif(num3>=num1 and num3>=num2 and num3>=num4):
+#   print("greatest num is:-",num3)
+# else:
+#    print("greatest num is:-",num4)
+
+# 2
+# sub1=int(input("enter your mark:-"))
+# sub2=int(input("enter your mark:-"))
+# sub3=int(input("enter your mark:-"))
+# if(sub1>=33 and sub2>=33 and sub3>=33):
+#   total=((sub1+sub2+sub3)/300)*100
+#   if(total>=40):
+#     print("you are pass")
+#   else:
+#     print("you are fail")
+# else:
+#   print("you are fail")
+
+# 3
+# txt=input("enter your comment:-")
+# if (("make a lot of money " in txt) or ("buy now " in txt) or ("subscribe this " in txt) or ("click this " in txt)):
+#   print("spam detected")
+# else:
+#   print("there is no spam")
+
+# 4
+# user=input("enter your name:-")
+# if (len(user)<10):
+#   print("given user has less than 10 character")
+# elif(len(user)==10):
+#    print("given user has 10 character")
+# else:
+#   print("given user has more than 10 character")
+
+# 5
+# list1=["rahul","himanshu","ankit","rohit"]
+# name=input("enter a name:-")
+# if name in list1:
+#   print("given name is present")
+# else:
+#   print("given name is not present")
+
+# 6
+# mark=int(input("enter your marks:-"))
+# if(mark>90 and mark<=100):
+#   grade="Ex"
+# elif(mark>80 and mark<=90):
+#   grade="A"
+# elif(mark>70 and mark<=80):
+#   grade="B"
+# elif(mark>60 and mark<=70):
+#   grade="C"
+# elif(mark>50 and mark<=60):
+#   grade="D"
+# else:
+#   grade="F"
+# print("your grade is:-",grade)
+
+# 7
+# post=input("enter your post:-")
+# if "harry".lower() in post.lower():
+#   print("harry is founded")
+# else:
+#   print("harry is not founded") 
+
+
+
