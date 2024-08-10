317039ea963db4c62e8c966ec9698ca21d624d01
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 01:59:56 +0530

    Add files via upload

---
+++ d2.py
@@ -0,0 +1,110 @@
+# a = 1
+# b = 2
+# c = 7
+# name = "aman"
+# print(a + b)
+
+
+# dadatype in python 
+# a = 1 # a is an integer
+# b = 5.22 # b is a floating point number
+# c = "Himanshu" # c is a string
+# d = False # d is a boolean variable
+# e = None # e is a none type variable
+
+# rule of varible 
+# a = 23
+# aaa = 435
+# harry = 34
+# sameer = 45
+# _samerr = 34
+
+# @amir = 56 # Invalid due to @ symbol
+# s@meer # Invalid due to @ symbol
+
+# operator in python 
+# Arithmetic Operators
+# a = 7
+# b = 4
+# c = a + b
+# print(c)
+
+# # Assignment Operators
+# a = 4-2 # Assign 4-2 in a
+# print(a)
+# b = 6
+# # b += 3 # Increment the value of b by 3 and then assign it to b
+# b -= 3 # Decrement the value of b by 3 and then assign it to b
+# print(b)
+
+# # Comparison Operators
+
+# d = 5==5
+
+# print(d)
+
+
+# # Logical Operators
+
+# e = True or False
+
+# # Truth table of 'or' 
+# print("True or False is ", True or False)
+# print("True or True is ", True or True)
+# print("False or True is ", False or True)
+# print("False or False is ", False or False)
+
+# # Truth table of 'and' 
+# print("True and False is ", True and False)
+# print("True and True is ", True and True)
+# print("False and True is ", False and True)
+# print("False and False is ", False and False)
+
+# print(not(True))
+
+# typcasting in python 
+# a = "31.2"
+# b = float(a) # a but the type should be float
+# t = type(b) 
+# print(t)
+
+# input in python 
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+print("Number a is: ", a)
+print("Number b is: ", b) 
+print("Sum is ", a + b)
+
+
+
+
+
+# practice set of day-2
+#1
+# num1=int(input("enter first number-:"))
+# num2=int(input("enter second number-:"))
+# print(num1+num2)
+
+#2
+# num1=int(input("enter first number-:"))
+# print("reminder is:-",num1%2)
+
+#3
+# num1=input("enter first number-:")
+# print(type(num1))
+
+#4
+# a=34
+# b=80
+# print(a>b)
+
+#5
+# num1=int(input("enter first number-:"))
+# num2=int(input("enter second number-:"))
+# avg=(num1+num2)/2
+# print("average of these two number is:-",avg)
+
+#5
+# num1=int(input("enter first number-:"))
+# sqr=num1**2
+# print("square of given number :-",sqr)
