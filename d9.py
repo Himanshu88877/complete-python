aac2daa569aaaf1755294ef268bfc9fb4e86f734
HIMANSHU <146114835+Himanshu88877@users.noreply.github.com>
2024-08-11 22:12:55 +0530

    Add files via upload

---
+++ d9.py
@@ -0,0 +1,70 @@
+# intro to file/IO 
+'''
+a = "a very long string with emails"
+
+emails = []
+3 seconds
+'''
+
+# f = open("file.txt", "r")
+# data = f.read()
+# print(data)
+# f.close()
+
+# how to write in file 
+# st = "Hey Harry you are amazing"
+# f = open("myfile.txt", "w")
+# f.write(st)
+# f.close()
+
+# more function on file 
+f = open("file.txt")
+
+# lines = f.readlines()
+# print(lines, type(lines))
+
+# line1 = f.readline()
+# print(line1, type(line1))
+
+# line2 = f.readline()
+# print(line2, type(line2))
+
+# line3 = f.readline()
+# print(line3, type(line3))
+
+# line4 = f.readline()
+# print(line4, type(line4))
+
+# line5 = f.readline()
+# print(line5 =="")
+# line = f.readline()
+# while(line != ""):
+#     print(line)
+#     line = f.readline()
+
+# f.close()
+
+# append mode 
+# st = "Hey Harry you are amazing"
+
+# f = open("myfile.txt", "a")
+
+# f.write(st)
+
+# f.close()
+
+# f = open("file.txt")
+# print(f.read())
+# f.close()
+
+# The same can be written using with statement like this:
+# with open("file.txt") as f:
+#     print(f.read())
+
+# You dont have to explicitly close the file
+
+# file.txt and myfile.txt is created 
+
+
+# practise set-9 
+
